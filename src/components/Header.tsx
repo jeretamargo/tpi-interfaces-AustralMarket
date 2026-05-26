768bc30897af836f23f92541eb03f617601c21c3
jeretamargo <jeretamargo01@gmail.com>
2026-05-26 12:57:57 -0300

    tipografías instaladas listas para usar

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="font-rubik font-extrabold">Austral Market</h1>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function Login() {
   return (
     <div>
-      
+      <h1 className="font-rubik font-bold">Austral Market</h1>
     </div>
-  )
+  );
 }
-
