4e3f75a7820c211c9d48ad62455b84073f9236d5
jeretamargo <jeretamargo01@gmail.com>
2026-05-26 13:20:39 -0300

    variables de tipografias y colores agregadas

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Login() {
   return (
     <div>
-      <h1 className="font-titulo font-extrabold">Austral Market</h1>
+      <h1 className="font-titulo font-extrabold text-naranja">
+        Austral Market
+      </h1>
     </div>
   );
 }
