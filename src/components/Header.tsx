f5fa1def01ccfb1eb181bf3b53df0d23bf519469
jeretamargo <jeretamargo01@gmail.com>
2026-05-26 13:28:46 -0300

    header empezado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="w-full px-5 h-16 bg-azul-oscuro flex ">
+      <h1 className="font-titulo text-2xl self-center font-extrabold text-naranja">
+        AUSTRAL MARKET
+      </h1>
+    </div>
+  );
 };
 
 export default Header;
