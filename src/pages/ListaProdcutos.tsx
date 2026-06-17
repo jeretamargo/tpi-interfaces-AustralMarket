e4936dce2402df48559993c8ce40aa39d1e6e67b
Sebastian Martinez <sebaamartinez54@gmail.com>
2026-06-17 09:39:54 -0300

    Fix: Se usa el mismo header que login

--- src/pages/ListaProdcutos.tsx
+++ src/pages/ListaProdcutos.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import HeaderLogeado from "../components/tabla/header-logeado";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
 import GestionProductos from "../components/tabla/Gestion";
 
 export default function ListaProdcutos() {
 	return (
-		<div>
-			<HeaderLogeado />
-			<GestionProductos />
-			<Footer />
-		</div>
+		<>
+			<header>
+				<Header />
+			</header>
+			<main>
+				<GestionProductos />
+			</main>
+			<footer>
+				<Footer />
+			</footer>
+		</>
 	);
 }
