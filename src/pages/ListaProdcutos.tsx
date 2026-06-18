3dfeeeb921a6924ba7d961ca4ea73f812d709ece
jeretamargo <jeretamargo01@gmail.com>
2026-06-18 09:44:01 -0300

    commit 2

--- src/pages/ListaProdcutos.tsx
+++ src/pages/ListaProdcutos.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import HeaderLogeado from "../components/tabla/header-logeado";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
 import GestionProductos from "../components/tabla/Gestion";
 
 export default function ListaProdcutos() {
-	return (
-		<div>
-			<HeaderLogeado />
-			<GestionProductos />
-			<Footer />
-		</div>
-	);
+  return (
+    <div>
+      <Header />
+      <GestionProductos />
+      <Footer />
+    </div>
+  );
 }
