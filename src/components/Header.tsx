8111af1f98faac45c7aa2c05422bb524b39cae7c
jeretamargo <jeretamargo01@gmail.com>
2026-06-18 13:32:10 -0300

    arreglos minimos de layout en login y nombre de usuario admin

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,9 +16,14 @@ const Header = () => {
             src="../../public/LOGO-AUSTRAL-MARKET.png"
             className="h-25"
           ></img>
-          <h1 className="hidden md:flex font-titulo text-2xl self-center font-extrabold text-azul">
-            AUSTRAL MARKET
-          </h1>
+          <div className="flex flex-col justify-center pl-2">
+            <h1 className="hidden md:flex font-titulo text-4xl self-center font-extrabold text-azul">
+              AUSTRAL
+            </h1>
+            <h1 className="hidden md:flex font-titulo text-4xl self-center font-extrabold text-azul">
+              MARKET
+            </h1>
+          </div>
         </div>
 
         <div className="flex flex-col md:flex-row items-center justify-center md:gap-2">
@@ -36,7 +41,7 @@ const Header = () => {
               d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
             />
           </svg>
-          <p className="text-black font-texto  ">Inicia Sesión</p>
+          <p className="text-black font-texto text-xl ">Inicia Sesión</p>
         </div>
       </div>
     );
@@ -48,9 +53,14 @@ const Header = () => {
             src="../../public/LOGO-AUSTRAL-MARKET.png"
             className="h-25"
           ></img>
-          <h1 className="hidden md:flex font-titulo text-2xl self-center font-extrabold text-azul">
-            AUSTRAL MARKET
-          </h1>
+          <div className="flex flex-col justify-center pl-2">
+            <h1 className="hidden md:flex font-titulo text-4xl self-center font-extrabold text-azul">
+              AUSTRAL
+            </h1>
+            <h1 className="hidden md:flex font-titulo text-4xl self-center font-extrabold text-azul">
+              MARKET
+            </h1>
+          </div>
         </div>
 
         <div className="flex gap-4 flex-row items-center justify-center ">
